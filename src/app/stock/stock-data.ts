765a1d0af15e400c346d072031dd3221db1a8baa
GowthamTuraga <45466464+GowthamTuraga@users.noreply.github.com>
2019-08-05 00:18:23 +0530

    Update stock-data.ts

--- src/app/stock/stock-data.ts
+++ src/app/stock/stock-data.ts
@@ -1,5 +1,6 @@
 import { Stock } from './stock';
 export const STOCK : Stock [] = [
-    {symbol:'INFY',prices:[10,20,30,40],company:'Infosys'},
-    {symbol:'APPL',prices:[50,60,70,80],company:'Apple'}
+    {symbol:'INFY',prices:[10,20,30,40],company:'Infosys',searchTime:''},
+    {symbol:'APPL',prices:[50,60,70,80],company:'Apple',searchTime:''},
+    {symbol:'TCS',prices:[70,65,95,45],company:'TCS',searchTime:''}
 ]
